c004baa6e258261e2682004bf8672b3ad5a8b617
reza107-hub <rashidaminur104@gmail.com>
2025-01-19 11:01:47 +0600

    image upload and link generate done using multer

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 
 const express = require('express');
 const cors = require('cors');
+const multer = require('multer');
+const path = require('path');
 
 const port = process.env.PORT || 4000;
 
@@ -10,11 +12,39 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Serve static files from 'uploads' directory
+app.use('/uploads', express.static('uploads')); // Add this line
+
+// Make sure uploads directory exists
+const fs = require('fs');
+if (!fs.existsSync('./uploads')) {
+    fs.mkdirSync('./uploads');
+}
+
+// upload image
+const storage = multer.diskStorage({
+    destination: './uploads',
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + path.extname(file.originalname));
+    }
+})
+
+const upload = multer({ storage });
+
 
 
 // api
 app.get('/', (req, res) => {
-  res.send('Hello World');
+    res.send('Hello World');
 });
 
-app.listen(port, ()=>{console.log(`Server is running on port ${port}`)});
+// image post method
+app.post('/api/upload-image', upload.single('image'), (req, res) => {
+    if (req.file) {
+        res.json({ imageUrl: `http://localhost:4000/uploads/${req.file.filename}` });
+    } else {
+        res.status(400).json({ error: 'No file uploaded' });
+    }
+});
+
+app.listen(port, () => { console.log(`Server is running on port ${port}`) });
